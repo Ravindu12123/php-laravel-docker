f653a054479e62bd9042b0d5219f38badfd0c0fa
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-12-24 19:18:34 +0530

    Update and rename g.php to pg.php

---
+++ public/pg.php
@@ -0,0 +1,21 @@
+<?php
+function file_get_contents_ssl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000); // 3 sec.
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10000); // 10 sec.
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+if (isset($_GET['url'])) {
+   echo file_get_contents_ssl($_GET['url']);
+}else{
+   echo "404";
+}
+?>
