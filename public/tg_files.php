073b02dbcfc0c326b25ad538c9a8de45dbd48fea
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-12-25 21:50:19 +0530

    Update tg_files.php

--- public/tg_files.php
+++ public/tg_files.php
@@ -4,10 +4,6 @@
   $M=$j['message'];$Uid=$M['from']['id'];
   $Mid=$M['message_id'];
    $ret='https://api.telegram.org/bot6906959412:AAFd2yMD7h6kDIUDSy7JkFOqKCYPeYtruxU/sendMessage?chat_id=-1002063159191&parse_mode=html';
-  if(isset($M['video']){
-     $ret=$ret.'&text=video';
-   }else{
-     $ret=$ret.'&text=non v';
-   }
+   $ret=$ret.'&text=hiiii';
  $nb= file_get_contents($ret);
 ?>
