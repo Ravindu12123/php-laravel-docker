374d552e7a9f93e5ef045f4ff4d611299e8278cb
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-12-25 21:12:39 +0530

    Update tg_files.php

--- public/tg_files.php
+++ public/tg_files.php
@@ -1,5 +1,15 @@
 <?php
   $in=file_get_contents("php://input");
-  $j=json_decode($in);
-  
+  $j=json_decode($in,true);
+  $M=$j['message'];$Uid=$M['from']['id'];
+  $Mid=$M['message_id'];
+   $ret = new \stdClass;
+   $ret->chat_id=$Uid;
+   $ret->parse_mode="html";
+   $ret->reply_to_message_id=$Mid;
+  if(isset($M['video']){
+     $ret->text="video";
+   }else{
+     $ret->text="non text";
+   }
 ?>
