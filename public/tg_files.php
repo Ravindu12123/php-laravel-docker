a08dc26196d5b1d4fffe09336b4b13e82b518d8e
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-12-30 18:28:00 +0530

    Update tg_files.php

--- public/tg_files.php
+++ public/tg_files.php
@@ -6,34 +6,35 @@
   $j=json_decode($in,true);
   $cc='-1002099678281';
   $botToken='6906959412:AAFd2yMD7h6kDIUDSy7JkFOqKCYPeYtruxU';
-  $M=$j['message'];$Uid=$M['from']['id'];
+  $M=$j['message'];
+  $Uid=$M['from']['id'];
   $Mid=$M['message_id'];
-   $ret='https://api.telegram.org/bot'.$botToken.'/sendMessage?chat_id='.$Uid.'&parse_mode=html';
+  $ret='https://api.telegram.org/bot'.$botToken.'/sendMessage?chat_id='.$Uid.'&parse_mode=html';
   $rett='https://api.telegram.org/bot'.$botToken.'/sendVideo?chat_id='.$cc.'&parse_mode=html';
-if($M['video']==null){
-       $ret=$ret.'&text=send me a video file';
+  if($M['video']==null){
+    $ret=$ret.'&text=send me a video file';
     $gh=file_get_contents($ret);
-   }else{
-       $nfid=$M['video']['file_id'];$ffar=$donefj['file_ids'];
-       if(in_array($nfid,$ffar,true)===true){
-           $ret=$ret.'&text=alredy done!&reply_to_message_id='.$Mid;
-           $nb= file_get_contents($ret);
-       }else{
-       $rett=$rett.'&video='.$M['video']['file_id'];
-       $nb= file_get_contents($rett);
+  }else{
+    $nfid=$M['video']['file_id'];
+    $ffar=$donefj['file_ids'];
+    if(in_array($nfid,$ffar,true)===true){
+        $ret=$ret.'&text=alredy done!&reply_to_message_id='.$Mid;
+        $nb=file_get_contents($ret);
+    }else{
+        $rett=$rett.'&video='.$M['video']['file_id'];
+        $nb= file_get_contents($rett);
         $nbr=json_decode($nb,true);
         if($nbr['ok']==true){
-        array_push($donefj['file_ids'],$M['video']['file_id']);
-       $donejj=json_encode($donefj);
-       file_put_contents($dataj,$donejj);
-       $ret=$ret.'&text=done!&reply_to_message_id='.$Mid;
-        $gh=file_get_contents($ret);
+            array_push($donefj['file_ids'],$M['video']['file_id']);
+            $donejj=json_encode($donefj);
+            file_put_contents($dataj,$donejj);
+            $ret=$ret.'&text=done!&reply_to_message_id='.$Mid;
+            $gh=file_get_contents($ret);
         }else{
             $ret=$ret.'&text=err!&reply_to_message_id='.$Mid;
             $gh=file_get_contents($ret);
         }
-    
-   }
-}
- 
+    }
+  }
+$sgsp="gg";
 ?>
