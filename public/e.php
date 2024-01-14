965bfef5022c1627ce5dbca4cc508ca63c752303
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-01-14 14:00:21 +0530

    Update e.php

--- public/e.php
+++ public/e.php
@@ -1,5 +1,14 @@
 <?php
-echo $_ENV['MY_VAR']."<br>";
-echo getenv('MY_VAR'); 
-
+$e1=echo $_ENV['MY_VAR'];
+$e2=echo getenv('MY_VAR'); 
+if(e1==null){
+echo "1nulll";
+}else{
+echo "1not";
+}
+if(e2==null){
+echo "2nulll";
+}else{
+echo "2not";
+}
 ?>
