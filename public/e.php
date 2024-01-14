50a502f1a667a15ec135af0179185f00150de826
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-01-14 14:12:08 +0530

    Update e.php

--- public/e.php
+++ public/e.php
@@ -3,12 +3,16 @@ $e1=$_ENV['MY_VAR'];
 $e2=getenv('MY_VAR'); 
 if($e1==null){
 echo "1nulll";
-}else{
+}else if($e1=="fucc"){
 echo "1not";
+}else{
+echo "1nooooo";
 }
 if($e2==null){
 echo "2nulll";
-}else{
+}else if($e2=="fucc"){
 echo "2not";
+}else{
+echo "2nooooo";
 }
 ?>
