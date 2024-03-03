8a119bcc378653cbc6cdc9a0a36dc205c6b3718f
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-03-03 09:32:38 +0530

    Update public

---
+++ public/sss.php
@@ -0,0 +1,42 @@
+<?php
+function postr($url,$data){   
+$content = json_encode($data);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+$json_response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+curl_close($curl);
+    return $json_response;
+}
+
+function gcssl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000); // 3 sec.
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10000); // 10 sec.
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+if(isset($_GET['t'])){
+
+}else{
+  echo 404;
+}
+
+
+?>
