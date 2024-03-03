3a2f0fd0660c91e3518039de596d20c41753a3d6
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-03-03 09:42:43 +0530

    Update sss.php

--- public/sss.php
+++ public/sss.php
@@ -38,7 +38,14 @@ if(isset($_GET['dw'])){
   $name=$_GET['n'];
   file_put_contents($name,$n);
   sleep(5000);
-  
+  $ret=postr('https://api.telegram.org/bot'.$_GET['token'].'/sendVideo',
+               array(
+                   'chat_id' => $_GET['chat_id'],
+                   'parse_mode' => 'html',
+                   'video' => 'https://phpttesrr.onrender.com/'.$n,
+                   'caption' => $cap
+                   ));
+    echo $ret;
 }else{
   echo 404;
 }
